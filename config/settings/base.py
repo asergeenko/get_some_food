876b94877592a667920b32d3d6f2ff2a985e8810
Aleksandr Sergeenko <asergeenko@gmail.com>
2021-12-03 17:40:48 +0300

    Product list

--- config/settings/base.py
+++ config/settings/base.py
@@ -42,7 +42,7 @@ LOCALE_PATHS = [str(ROOT_DIR / "locale")]
 # https://docs.djangoproject.com/en/dev/ref/settings/#databases
 DATABASES = {"default": env.db("DATABASE_URL")}
 DATABASES["default"]["ATOMIC_REQUESTS"] = True
-DATABASES["default"]["ENGINE"] = 'django.db.backends.postgresql_psycopg2'
+#DATABASES["default"]["ENGINE"] = 'django.db.backends.postgresql_psycopg2'
 
 # URLS
 # ------------------------------------------------------------------------------
@@ -94,7 +94,7 @@ MIGRATION_MODULES = {"sites": "get_some_food.contrib.sites.migrations"}
 # https://docs.djangoproject.com/en/dev/ref/settings/#authentication-backends
 AUTHENTICATION_BACKENDS = [
     "django.contrib.auth.backends.ModelBackend",
-    "allauth.account.auth_backends.AuthenticationBackend",
+#    "allauth.account.auth_backends.AuthenticationBackend",
 ]
 # https://docs.djangoproject.com/en/dev/ref/settings/#auth-user-model
 AUTH_USER_MODEL = "users.User"
@@ -109,9 +109,9 @@ LOGIN_URL = "account_login"
 PASSWORD_HASHERS = [
     # https://docs.djangoproject.com/en/dev/topics/auth/passwords/#using-argon2-with-django
     "django.contrib.auth.hashers.Argon2PasswordHasher",
-    "django.contrib.auth.hashers.PBKDF2PasswordHasher",
-    "django.contrib.auth.hashers.PBKDF2SHA1PasswordHasher",
-    "django.contrib.auth.hashers.BCryptSHA256PasswordHasher",
+    #"django.contrib.auth.hashers.PBKDF2PasswordHasher",
+    #"django.contrib.auth.hashers.PBKDF2SHA1PasswordHasher",
+    #"django.contrib.auth.hashers.BCryptSHA256PasswordHasher",
 ]
 # https://docs.djangoproject.com/en/dev/ref/settings/#auth-password-validators
 AUTH_PASSWORD_VALIDATORS = [
